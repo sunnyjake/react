e23b326924abc8ad3587443b896f94f417e40fec
sunnyjake <evgenij.jenya@yandex.ru>
2016-12-24 14:50:38 +0200

    Used ES6-syntax to write components

--- js/app.js
+++ js/app.js
@@ -23,41 +23,92 @@ var articles = [
     }
 ];
 
-// class Article extends React.Component{
-
-// }
-
-var Article = React.createClass({
-    propTypes: {
-        data: React.PropTypes.shape({
-            author: React.PropTypes.string.isRequired,
-            text: React.PropTypes.string.isRequired,
-            bigText: React.PropTypes.string.isRequired
-        })
-    },
-    getInitialState: function () {
-        return {
+class Article extends React.Component{
+    constructor(){
+        super();
+        this.state = {
             visible: false
-        }
-    },
-    readMoreClick: function (e) {
+        };
+        this.readMoreClick = this.readMoreClick.bind(this);
+    }
+    readMoreClick(e){
         e.preventDefault();
-        // this.state.visible = true;
-        this.setState({ visible: true });
-    },
-    render: function () {
-        var visible = this.state.visible;
-        // console.log(this.props.data);
-        return (
+        console.log(this);
+        this.setState({visible: true})
+    }
+    // componentWillMount(){
+    //     console.log("mount");
+    // }
+    // componentDidMount(){
+    //     console.log("did mount")
+    // }
+    render(){
+        return(
             <div className="article">
                 <div className="author">{this.props.data.author}</div>
                 <div className={'text ' + (this.state.visible ? 'hidden' : '')}>{this.props.data.text + "... "}</div>
                 <a href="#" className={"linkShowMore " + (this.state.visible ? 'hidden' : '')} onClick={this.readMoreClick}>show more</a>
                 <div className={"text_big " + (!this.state.visible ? 'hidden' : '')}>{this.props.data.bigText}</div>
-            </div>
+            </div> 
         )
     }
-});
+}
+Article.propTypes = {
+        data: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    }
+
+class Text extends Article {
+    constructor(){
+        super();
+        this.readMoreClick = this.readMoreClick.bind(this);
+    }
+    readMoreClick(e){
+        super.readMoreClick(e);
+        console.log("text", this);
+    }
+    render(){
+        return(
+            <div>
+                <p onClick={this.readMoreClick}>Text</p>
+                </div>
+        )
+    }
+}
+
+// var Article = React.createClass({
+//     propTypes: {
+//         data: React.PropTypes.shape({
+//             author: React.PropTypes.string.isRequired,
+//             text: React.PropTypes.string.isRequired,
+//             bigText: React.PropTypes.string.isRequired
+//         })
+//     },
+//     getInitialState: function () {
+//         return {
+//             visible: false
+//         }
+//     },
+//     readMoreClick: function (e) {
+//         e.preventDefault();
+//         // this.state.visible = true;
+//         this.setState({ visible: true });
+//     },
+//     render: function () {
+//         var visible = this.state.visible;
+//         return (
+//             <div className="article">
+//                 <div className="author">{this.props.data.author}</div>
+//                 <div className={'text ' + (this.state.visible ? 'hidden' : '')}>{this.props.data.text + "... "}</div>
+//                 <a href="#" className={"linkShowMore " + (this.state.visible ? 'hidden' : '')} onClick={this.readMoreClick}>show more</a>
+//                 <div className={"text_big " + (!this.state.visible ? 'hidden' : '')}>{this.props.data.bigText}</div>
+//             </div>
+//         )
+//     }
+// });
 
 var News = React.createClass({
     propTypes: {
@@ -75,6 +126,7 @@ var News = React.createClass({
         return (
             <div className="news">
                 {articles}
+                <Text />
                 <strong>{'There are ' + this.props.data.length + ' news'}</strong>
             </div>
         )
@@ -93,19 +145,67 @@ var TestInput = React.createClass({
         // console.log(e.currentTarget.value);
         this.setState({ model: e.currentTarget.value });
     },
+    liveSearchWithoutRender: function(e){
+        console.log(ReactDOM.findDOMNode(this.refs.myTestInput).value);
+    },
     getAlert: function(){
         alert(this.state.model);
     },
+    componentDidMount: function(){
+        ReactDOM.findDOMNode(this.refs.myTestInput).focus();
+    },
     render: function () {
+        console.log("render", this);
         return (
             <div>
                 <input className="textInput" value={this.state.model} onChange={this.liveSearch} id="test" placeholder="test" />
+                <input defaultValue='' placeholder='Text' onChange={this.liveSearchWithoutRender} ref='myTestInput' />
                 <input type="button" value="Test" onClick={this.getAlert} />
                 <span>{this.state.model}</span>
             </div>
         )
     }
-})
+});
+
+class Add extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            disabled: true
+        };
+        this.buttonEnable = this.buttonEnable.bind(this);
+    }
+    buttonEnable(e){
+        if(e.target.checked){
+            this.setState({disabled: false});
+        }
+        else{
+            this.setState({disabled: true});
+        }
+        
+    }
+    render(){
+        return(
+            <div className='addNew'>
+                <p>Add new article</p>
+                <form className='add cf'>
+                    <div className='form-group'>
+                        <label htmlFor='name'>Author</label>
+                    <input type='text' className='add__author' placeholder='Name' id='name' />
+                    </div>
+                    <div className='form-group'>
+                    <label htmlFor='text'>Article</label>
+                    <textarea id='text' />
+                    </div>
+                    <div className='form-group'>
+                    <input type='checkbox' className='add__text' id='agree' onChange={this.buttonEnable} /><label htmlFor='agree'>I am agree with privacy policy</label>
+                    </div>
+                    <input type='button' className='add__btn' value='Add article' disabled={this.state.disabled ? true : false} />
+                </form>
+            </div>
+        )
+    }
+}
 
 var App = React.createClass({
     render: function () {
@@ -113,6 +213,7 @@ var App = React.createClass({
             <div className="container-fluid">
                 <TestInput />
                 <News data={articles} />
+                <Add />
             </div>
         );
     }
